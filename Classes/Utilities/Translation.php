210b02a185f84dcf00e3a5b8387beba78d42e003
Christian Händel <c.haendel@division.ag>
2018-05-02 10:52:12 +0200

    [ADD] translation service [REMOVE] old unused single files for regions types tastes grapetypes and countrycodes

--- Classes/Utilities/Translation.php
+++ Classes/Utilities/Translation.php
@@ -1,5 +1,5 @@
 <?php
-namespace Vinou\Translation\Utilities;
+namespace Vinou\Translations\Utilities;
 
 
 /**
@@ -33,28 +33,104 @@ class Translation {
 	}
 
 	public function getRegion($id){
-		return $this->data['regions'][$id];
+		if (isset($this->data['regions'][$id])) {
+			return $this->data['regions'][$id];
+		} else {
+			return false;
+		}
 	}
 
 	public function getType($type){
-		return $this->data['regions'][$type];
+		if (isset($this->data['winetypes'][$type])) {
+			return $this->data['winetypes'][$type];
+		} else {
+			return false;
+		}
 	}
 
 	public function getTaste($id){
-		return $this->data['tastes'][$id];
+		if (isset($this->data['tastes'][$id])) {
+			return $this->data['tastes'][$id];
+		} else {
+			return false;
+		}
 	}
 
 	public function getGrapeType($id){
-		return $this->data['grapetypes'][$id];
+		if (isset($this->data['grapetypes'][$id])) {
+			return $this->data['grapetypes'][$id];
+		} else {
+			return false;
+		}
 	}
 
 	public function getValueByKey($key,$id){
-		return $this->data[$key][$id];
+		if (isset($this->data[$key][$id])) {
+			return $this->data[$key][$id];
+		} else {
+			return false;
+		}
 	}
 
-	public function getFullFile($key = NULL) {
-		is_null($key) ? $key = $this->countryCode : $key = $key;
-		return file_get_contents($this->llPath.$key.'.json');
+	public function get($countryCode = NULL,$selector = NULL) {
+		is_null($countryCode) ? $countryCode = $this->countryCode : $countryCode = $countryCode;
+		$all = json_decode(file_get_contents($this->llPath.$countryCode.'.json'),TRUE);
+		if (is_null($selector)) {
+			return $all;
+		} else {
+			$returnArr = $this->findKeyInArray($selector,$all);
+			// if (is_string($returnArr)) $returnArr = ['value' => $returnArr];
+			return $returnArr;
+		}
+	}
+
+	/**
+	 *
+	 * localize wine
+	 *
+	 * @param array $wine
+	 * @return array
+	 */
+	public function localizeWine($wine = NULL) {
+		foreach ($wine as $property => $value) {
+			switch ($property) {
+				case 'grapetypes':
+					if (!empty($value)) {
+						$grapetypes = [];
+						foreach ($value as $grapetype) {
+							if ($this->getGrapeType($grapetype) !== false) {
+								$grapetypes[$grapetype] = $this->getGrapeType($grapetype);
+							}
+						}
+						$wine[$property] = $grapetypes;
+					}
+					break;
+				case 'type':
+					if ($this->getType($value) !== false) $wine[$property] = $this->getType($value);
+					break;
+				case 'tastes_id':
+					if ($this->getTaste($value) !== false) $wine[$property] = $this->getTaste($value);
+					break;
+				case 'region':
+					if ($this->getRegion($value) !== false) $wine[$property] = $this->getRegion($value);
+					break;
+				default:
+					$wine[$property] = $value;
+					break;
+			}
+		}
+		return $wine;
+	}
+
+
+	protected function findKeyInArray($keyArray,$array) {
+		$searchArray = $array;
+		foreach ($keyArray as $key) {
+			if (isset($searchArray[$key])) {
+				$searchArray = $searchArray[$key];
+			}
+		}
+		return $searchArray;
 	}
 
 }
